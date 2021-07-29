834c2668a9345a8c6c919ebaa87e650206df87cd
khulanJargalsaikhan <onagraa@gmail.com>
2021-07-29 18:57:37 -0400

    second commit

--- src/main/java/com/example/todolist/HomeController.java
+++ src/main/java/com/example/todolist/HomeController.java
@@ -1,10 +1,14 @@
 package com.example.todolist;
 
+import org.omg.CORBA.FieldNameHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 @Controller
 public class HomeController {
@@ -23,6 +27,7 @@ public class HomeController {
     @PostMapping("/lists")
     public String loadList(@ModelAttribute ToDo todo, Model model){
         lists.add(todo);
+        //sortList(lists);
         model.addAttribute("lists", lists);
         return "lists";
     }
@@ -38,6 +43,21 @@ public class HomeController {
         todo.setId(idCounter);
     }
 
+//    static ArrayList sortList(ArrayList lists){
+//        Collections.sort(lists);
+//        return lists;
+//    }
+
+    @RequestMapping("/list/{id}")
+    public String loadDetail(@PathVariable("id") long id, Model model){
+        for (ToDo todo: lists){
+            if(id == todo.getId()){
+                model.addAttribute("todo", todo);
+            }
+        }
+        return "list";
+    }
+
     @RequestMapping("/update/{id}")
     public String updateTodolist(@PathVariable("id") long id, Model model){
         for (ToDo todo: lists){
@@ -47,4 +67,19 @@ public class HomeController {
         }
         return "add";
     }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteTodo(@PathVariable("id") long id, Model model){
+        for (ToDo todo: lists){
+            if(id == todo.getId()){
+                //int todoIndex = lists.indexOf(todo);
+                lists.remove(todo);
+                break;
+            }
+        }
+        return "delete";
+    }
+
+
+
 }
